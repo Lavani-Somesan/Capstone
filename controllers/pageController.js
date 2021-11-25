faae52ec1b4daa680d2983b4a0882b968ba6b66f
lks17 <lks17@students.uwf.edu>
2021-11-24 23:57:40 -0600

    Fixed user from relogging in or creating a new account when logged in

--- controllers/pageController.js
+++ controllers/pageController.js
@@ -4,18 +4,33 @@ exports.getHomePage = (req, res) => {
 }
 
 exports.getLoginPage = (req, res) => {
-    
-    res.render("login", {session : req.session.user_ApiToken});
+
+    if (req.session.user_ApiToken) {
+        req.flash("error", "Already Logged In!");
+        res.redirect("/user/profile");
+    } else {
+        res.render("login", {session : req.session.user_ApiToken});
+    }
 }
 
 exports.getAccountCreationPage = (req, res) => {
-    
-    res.render("accountCreation", {session : req.session.user_ApiToken});
+
+    if (req.session.user_ApiToken) {
+        req.flash("error", "Error, Cannot Create an Account Since You Are Logged In!");
+        res.redirect("/user/profile");
+    } else {
+        res.render("accountCreation", {session : req.session.user_ApiToken});
+    }
 }
 
 exports.getAccountGuidePage = (req, res) => {
     
-    res.render("accountGuide", {session : req.session.user_ApiToken});
+    if (req.session.user_ApiToken) {
+        req.flash("error", "Error, Cannot Create an Account or LogIn Since You Are Logged In!");
+        res.redirect("/user/profile");
+    } else {
+        res.render("accountGuide", {session : req.session.user_ApiToken});
+    }
 }
 
 exports.getAboutPage = (req, res) => {
